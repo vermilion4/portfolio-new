f1e0b5a43b176235db937def7fb35f6993af3e83
Adaeze Ndupu <andupu4@gmail.com>
2024-01-18 17:07:47 +0100

    feat: change services section

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -1,44 +1,43 @@
-import Image from "next/image";
+import { services } from '@/data/services';
+import Image from 'next/image';
 
 const Services = () => {
-  return ( 
-    <section className="max-w-7xl mx-auto pt-14 pb-20 flex flex-col items-center px-5 sm:px-10">
-      <h2 className="text-2xl font-bold tracking-[0.72px] mb-4">My Services</h2>
-      <p className="text-white opacity-30 text-sm tracking-[0.42px] mb-12">Services I offer to my clients</p>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 w-2/3 md:w-4/5">
-        <div className="text-white text-sm pb-14 lg:pb-0 lg:pr-14 lg:border-r border-b lg:border-b-0 border-white border-opacity-10 flex flex-col items-center md:items-start">
-          <Image src="/service1.svg" alt="service image" width={57} height={57} />
-          <h3 className="text-lg mb-2 mt-[23px]">What I can do for you</h3>
-          <p className="mb-[11px] opacity-70 text-center md:text-start">Bring products to life, using a user-centered design approach. Here are the services i provide:</p>
-          <ul className="opacity-70 list-disc space-y-3 list-inside">
-            <li>UI/UX Design</li>
-            <li>Graphics Design</li>
-            <li>Brand Design</li>
-          </ul>
-        </div>
-        <div className="text-white text-sm py-14 md:pt-0 md:pb-14 md:pl-14 lg:px-14 lg:pb-0 lg:border-r border-b lg:border-b-0 border-white border-opacity-10 flex flex-col items-center md:items-start">
-        <Image src="/service2.svg" alt="service image" width={55} height={55} />
-          <h3 className="text-lg mb-2 mt-[23px]">Applications I am fluent in</h3>
-          <p className="mb-[11px] opacity-70 text-center md:text-start">Every designer requires the right applications to bring their vision to life, and my go-to choices include</p>
-          <ul className="opacity-70 list-disc space-y-3 list-inside">
-            <li>Figma</li>
-            <li>Adobe Creative Suite</li>
-            <li>Canva</li>
-          </ul>
-        </div>
-        <div className="text-white text-sm pt-14 lg:pt-0 lg:pl-14 flex flex-col items-center md:items-start">
-        <Image src="/service3.svg" alt="service image" width={48} height={48} />
-          <h3 className="text-lg mb-2 mt-[23px]">What you can expect</h3>
-          <p className="mb-[11px] opacity-70 text-center md:text-start">My focus is on more than just creating stunning products; it&apos;s about prioritizing exceptional user experiences. Users first, design second</p>
-          <ul className="opacity-70 list-disc space-y-3 list-inside">
-            <li>Innovative and useable products</li>
-            <li>Creative visual designs</li>
-            <li>distinctive brand identities</li>
-          </ul>
-        </div>
+  return (
+    <section className='max-w-7xl mx-auto pt-14 pb-20 flex flex-col items-center px-5 sm:px-10'>
+      <h2 className='text-2xl font-bold tracking-[0.72px] mb-4'>My Services</h2>
+      <p className='text-white opacity-30 text-sm tracking-[0.42px] mb-12'>
+        Services I offer to my clients
+      </p>
+      <div className='grid grid-flow-row md:grid-cols-2 xl:grid-cols-3 w-[85%] md:w-11/12 gap-[55px]'>
+        {services.map(({ id, title, tools }) => (
+          <div
+            key={id}
+            className='text-center py-[50px] px-[65.5px] bg-white bg-opacity-[4%] border border-transparent duration-300 hover:border-[#FFFFFF1A] rounded-[6px] hover:bg-primary hover:bg-opacity-40 transition-all ease-out'>
+            <h4 className='mb-4 font-bold'>{title}</h4>
+            <p className='text-white text-opacity-40 text-sm mb-[26px]'>
+              Tools used to achieve this
+            </p>
+            <div className='flex flex-wrap gap-[18px] justify-center'>
+              {tools.map(({ id, name, icon }) => (
+                <div key={id} className='flex gap-[6px] items-center'>
+                  <div>
+                    <Image
+                      width={18}
+                      height={18}
+                      src={icon}
+                      alt={name}
+                      layout='responsive'
+                    />
+                  </div>
+                  <p className='text-white text-opacity-70 text-sm'>{name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
-   );
-}
- 
+  );
+};
+
 export default Services;
---
+++ src/data/services.js
@@ -0,0 +1,39 @@
+export const services = [{
+  id: 1,
+  title: 'UI/UX Design',
+  tools: [{
+    id: 10,
+    name: 'Figma',
+    icon: '/figma.svg'
+  }]
+},
+{
+  id: 2,
+  title: 'Graphic Design',
+  tools: [{
+    id: 20,
+    name: 'Figma',
+    icon: '/figma.svg'
+  },
+  {
+    id: 30,
+    name: 'Photoshop',
+    icon: '/photoshop.svg'
+  }
+  ]
+},
+{
+  id: 2,
+  title: 'Brand Design',
+  tools: [{
+    id: 40,
+    name: 'Figma',
+    icon: '/figma.svg'
+  },
+  {
+    id: 50,
+    name: 'Photoshop',
+    icon: '/photoshop.svg'
+  }]
+}
+];
