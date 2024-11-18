9d5c62b70b341505a42cab80959cfa60bbd0c91d
Adaeze Ndupu <andupu4@gmail.com>
2024-11-18 04:28:45 +0100

    feat: setup gh pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: '/portfolio-new/',
+  basePath: '/portfolio-new',
+  output: 'export'
 }
 
 module.exports = nextConfig
